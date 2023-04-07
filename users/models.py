e0cb560bd5b556b8ba3c64ef311ee47750b97f2a
apolumestny <anpolumestny@gmail.com>
2023-04-07 14:08:31 +0400

    replase default django user model

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    email = models.EmailField(blank=False)
+    first_name = models.CharField(max_length=120, blank=False)
+    last_name = models.CharField(max_length=120, blank=False)
