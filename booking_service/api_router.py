8acde941112be2b03c80e469d89110df93d9040f
apolumestny <anpolumestny@gmail.com>
2023-04-27 12:30:44 +0300

    add order api

--- booking_service/api_router.py
+++ booking_service/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from halls.api.views import HallTypeViewSet, PropertyViewSet, HallViewSet
+from orders.api.views import OrderView, OrderStatusView, OrderHistoryView
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -11,5 +12,7 @@ else:
 router.register('hall-type', HallTypeViewSet)
 router.register('property', PropertyViewSet)
 router.register('hall', HallViewSet)
-
+router.register('order', OrderView)
+router.register('order-status', OrderStatusView)
+router.register('order-history', OrderHistoryView)
 urlpatterns = router.urls
