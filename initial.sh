ab0b5f9bd7a621f7f4dee61236c38045ba21bd47
apolumestny <anpolumestny@gmail.com>
2023-04-30 23:02:28 +0300

    add approved order date to hall serializer. Add creating static folder in django start script, add command create_orders, add create orders command in initial script

--- initial.sh
+++ initial.sh
@@ -6,3 +6,6 @@ python manage.py load_property_type --file internal_files/type_properties.csv
 
 # load order status
 python manage.py load_order_status --file internal_files/order_status.csv
+
+# create orders
+python manage.py create_orders
